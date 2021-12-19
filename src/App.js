a9dc89bac3a9469514e4452b30875c7ba1357a91
waelsta <waelsta099@gmail.com>
2021-12-19 13:28:54 +0100

    created project from create react app and added files from homepage

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import HomePage from './homepage.component';
+import './homepage.style.scss';
+
+function App() {
+  return (
+    <div>
+      <HomePage/>
+    </div>
+  );
+}
+
+export default App;
